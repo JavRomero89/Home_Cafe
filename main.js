74d19a41c76072d4700fb570822a270bcd7be781
JavRomero89 <javieromero23@gmail.com>
2023-08-28 00:24:30 +0200

    advance

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-const products = async () => {
-    const response = await fetch("https://cafe-de-altura.vercel.app/api/products");
-    const data = await response.json();
- return data    
+// const products = async () => {
+//     const response = await fetch("https://cafe-de-altura.vercel.app/api/products");
+//     const data = await response.json();
+//  return data    
 
+// }
+
+// const getProducts = async() => {
+//     const productos = await products();
+
+//     console.log(productos);
+// }
+
+// getProducts();
+
+function iniciarSesion() {
+    window.location.href = "./login/index.html"; 
 }
-
-const getProducts = async() => {
-    const productos = await products();
-
-    console.log(productos);
-}
-
-getProducts();
-
-
-
+document.getElementById("login").addEventListener("click", iniciarSesion);
 
 
